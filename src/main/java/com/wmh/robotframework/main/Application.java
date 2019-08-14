269bbb16c9ddff60ebe33b98fff8a78b2fc7a762
unknown <2570230521@qq.com>
2019-08-15 02:49:49 +0800

    springboot

--- src/main/java/com/wmh/robotframework/main/Application.java
+++ src/main/java/com/wmh/robotframework/main/Application.java
@@ -1,7 +1,33 @@
 package com.wmh.robotframework.main;
 
-public class Application {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
-    }
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		final CountDownLatch countDownLatch = new CountDownLatch(1);
+		Thread holderThread = new Thread(() -> {
+			ApplicationContext applicationContext = new SpringApplicationBuilder()
+					.web(WebApplicationType.NONE)
+					.sources(Application.class)
+					.run(args);
+			Logger logger = LoggerFactory.getLogger(Application.class);
+			try {
+				logger.info("holder thread started ...");
+				countDownLatch.await();
+			} catch (InterruptedException e) {
+				logger.error("holder thread Interrupted", e);
+			}
+		});
+		holderThread.setName("ApplicationThread");
+		holderThread.start();
+	}
 }
--- src/test/java/com/wmh/robotframework/test/RobotFrameworkMojoTest.java
+++ src/test/java/com/wmh/robotframework/test/RobotFrameworkMojoTest.java
@@ -6,6 +6,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.File;
 
 public class RobotFrameworkMojoTest {
+
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         RobotFrameworkMojo robotFrameworkMojo = new RobotFrameworkMojo();
