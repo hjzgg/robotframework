cceaa7274c3925c7a0e2311a163ea63a4b3b5378
unknown <2570230521@qq.com>
2019-08-19 18:33:04 +0800

    fix bug

--- src/main/java/com/wmh/robotframework/config/CommonConfig.java
+++ src/main/java/com/wmh/robotframework/config/CommonConfig.java
@@ -2,26 +2,23 @@ package com.wmh.robotframework.config;
 
 import com.wmh.robotframework.gui.GUIDemo;
 import com.wmh.robotframework.log.ILogService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-	@Autowired
-	GUIDemo guiDemo;
+    @Autowired
+    GUIDemo guiDemo;
 
-	@Bean
-	public ILogService logService() {
-
-		return new ILogService() {
-			@Override
-			public void log(String caseId, String message) {
-				System.out.println("------------" + (null == guiDemo));
+    @Bean
+    public ILogService logService() {
+        return (caseId, message) -> {
+			synchronized (this) {
 				guiDemo.logTextArea.append(message);
+				guiDemo.logTextArea.append(System.lineSeparator());
 				guiDemo.logTextArea.paintImmediately(guiDemo.logTextArea.getBounds());
 			}
-		};
-	}
+        };
+    }
 }
