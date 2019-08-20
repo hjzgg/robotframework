3d019f90ab994852735cebd4071669fc5a5d72a3
unknown <2570230521@qq.com>
2019-08-20 08:22:46 +0800

    fix bug

--- src/main/java/com/wmh/robotframework/service/TestService.java
+++ src/main/java/com/wmh/robotframework/service/TestService.java
@@ -37,42 +37,7 @@ public class TestService implements LoggerAdapter {
     @Autowired
     private BrowserExportProperties browserExportProperties;
 
-
     public void execute(TestProperties tp) {
-        String testCaseId = tp.getTestCaseId();
-        if (StringUtils.isBlank(tp.getDebugLogFile())) {
-            try {
-                File tmpFile = Files.createTempFile(Files.createTempDirectory(testCaseId), "debug", ".log").toFile();
-                tp.setDebugLogFile(tmpFile.getAbsolutePath());
-            } catch (IOException e) {
-                LOGGER.error("DEBUG LOG FILE CREATE ERROR...", e);
-            }
-        }
-
-        BrowserDriverManager browserDriverManager = BrowserDriverManager.getInstance(tp.getDriverManagerType());
-        if (browserDriverManager.canUseMirror()) {
-            browserDriverManager.useMirror();
-        }
-        if (StringUtils.isNoneBlank(tp.getDriverVersion())) {
-            browserDriverManager.version(tp.getDriverVersion());
-        }
-        browserDriverManager.setup();
-        String browserDriverPath = browserDriverManager.getBinaryPath();
-        System.setProperty(browserExportProperties.resovleExportPath(tp.getDriverManagerType()), browserDriverPath);
-        LOGGER.info("load browser driver path: " + browserDriverPath);
-
-        RobotFrameworkMojo robotFrameworkMojo = new RobotFrameworkMojo();
-        LOGGER.error("TEST CASE PATH IS " + tp.getTestCasesDirectory());
-        robotFrameworkMojo.setTestCasesDirectory(new File(tp.getTestCasesDirectory()));
-        LOGGER.error("TEST REPORT PATH IS " + tp.getTestCasesDirectory());
-        robotFrameworkMojo.setOutputDirectory(new File(tp.getOutputDirectory()));
-        LOGGER.error("TEST DEBUG LOG PATH IS " + tp.getDebugLogFile());
-        robotFrameworkMojo.setDebugFile(new File(tp.getDebugLogFile()));
-        robotFrameworkMojo.execute();
-
-    }
-
-    public void executex(TestProperties tp) {
         String testCaseId = tp.getTestCaseId();
         if (StringUtils.isBlank(tp.getDebugLogFile())) {
             try {
